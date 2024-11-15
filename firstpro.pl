f38e970166a1799379059a1eac5dbb8119038703
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-11-15 21:10:07 +0530

    update

--- firstpro.pl
+++ firstpro.pl
@@ -5,3 +5,9 @@ likes(mary,food).
 likes(mary,wine).
 likes(john,wine).
 likes(john,mary).
+
+likes(john,X) :- likes(mary,X).
+
+likes(john,Person) :- likes(Person,wine).
+
+likes(john,Person) :- likes(Person,Person).
